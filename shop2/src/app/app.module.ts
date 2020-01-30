10ebb29e95944583d51117f8ecb212534deb723e
Veranika Kachubeyeva <Veranika_Kachubeyeva@epam.com>
2020-01-30 10:43:57 +0300

    products

--- shop2/src/app/app.module.ts
+++ shop2/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrdersModule } from './orders/orders.module';
+import { CartModule } from './cart/cart.module';
+import { SharedModule } from './shared/shared.module';
+import { ProductsModule } from './products/products.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CartModule,
+    ProductsModule,
+    SharedModule,
+    OrdersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
